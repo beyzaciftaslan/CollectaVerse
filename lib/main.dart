971960af4ca60ace8615e59908ab4e5b18fac79b
Beyza Çiftaslan <72284155+beyzaciftaslan@users.noreply.github.com>
2024-04-29 10:27:08 +0300

    themes created
    light mode n dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,20 @@
-import 'package:collecta_verse_pt2/landing_page.dart';
+//import 'package:collecta_verse_pt2/landing_page.dart';
+import 'package:collecta_verse_pt2/pages/login_page.dart';
+import 'package:collecta_verse_pt2/theme/light_mode.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: LandingPage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-          ],
-        ),
-      ),
+      home: LoginPage(),
+      theme: lightMode,
     );
   }
 }
